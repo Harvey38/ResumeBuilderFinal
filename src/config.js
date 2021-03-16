e3184ca0003931e901588ef6218a078968842219
Harv38 <tushar.gupta@scalenut.com>
2021-03-16 13:59:20 +0530

    done

---
+++ src/config.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+var firebaseConfig = {
+    apiKey: "AIzaSyBoGvmfSf0pXzGsLB_5v-DKn3PgfQNw1BQ",
+    authDomain: "resume-builder-19639.firebaseapp.com",
+    projectId: "resume-builder-19639",
+    storageBucket: "resume-builder-19639.appspot.com",
+    messagingSenderId: "925394018410",
+    appId: "1:925394018410:web:3050063bd59db4027bea13"
+};
+// Initialize Firebase
+firebase.initializeApp({
+    apiKey: "AIzaSyBoGvmfSf0pXzGsLB_5v-DKn3PgfQNw1BQ",
+    authDomain: "resume-builder-19639.firebaseapp.com",
+    projectId: "resume-builder-19639",
+    storageBucket: "resume-builder-19639.appspot.com",
+    messagingSenderId: "925394018410",
+    appId: "1:925394018410:web:3050063bd59db4027bea13"
+});
+
+export default firebase;
